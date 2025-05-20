52a43ce819ebbe6d660fe058fae604f0679ca279
gtguch <pubguchd@gmail.com>
2025-05-20 13:36:59 +0900

    [Add] markovpppO

---
+++ markovpppO.m
@@ -0,0 +1,40 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% A script to evaluate the proportion of preceding particles
+% ppp: the proportion of preceding particles
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% clear all parameters and close all figures
+clear;
+close all;
+
+% set parameter values
+tp = .5;        % transition probability between the two states
+n = 8000;       % the number of sample paths
+l = 2000;       % time length for calculation
+Du = 1;         % diffusion coefficient at + state
+Dd = 0.2;       % diffusion coefficient at - state
+res = 0.01;     % time resolution
+maxid = 1000;   % time length
+
+% calculate the proportion of preceding particles
+[Dc, leng] = tsMDudO(tp,n,l,Du,Dd,res,maxid);
+
+tt = res:res:maxid;
+Dm = (Du + Dd)/2;
+hp = Du/Dm;
+hm = Dd/Dm;
+
+D1 = cell2mat(Dc);
+D2 = reshape(D1,maxid/res,[]);
+D = D2';
+clear D1 D2;
+
+nD = D/Dm; 
+
+H = taveragDO(nD, leng);
+erfcH = erfc((2)./sqrt(H));
+ppp = mean(erfcH);
+
+
+
+
